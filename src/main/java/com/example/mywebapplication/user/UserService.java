cd7f2cdd769de4504d9683de94aad46b122a487f
Isabelle Bustamante <isabelle.bustamante@students.fhnw.ch>
2022-11-21 21:19:53 +0100

    Update UserService.java

--- src/main/java/com/example/mywebapplication/user/UserService.java
+++ src/main/java/com/example/mywebapplication/user/UserService.java
@@ -1,2 +1,35 @@
-package com.example.mywebapplication.user;public class UserService {
+package com.example.mywebapplication.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.attribute.UserPrincipalNotFoundException;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired private UserRepository repo;
+
+    public List<User> listAll() {
+        return (List<User>) repo.findAll();
+
+    }
+
+    public void save(User user) {
+
+        repo.save(user);
+    }
+
+    public User get(Integer id) throws UserNotFoundException{
+        Optional<User> result = repo.findById(id);
+        if (result.isPresent()){
+            return (User) result.get();
+        }
+
+        throw new UserNotFoundException("could not find any users with ID");
+
+
+    }
 }
+
