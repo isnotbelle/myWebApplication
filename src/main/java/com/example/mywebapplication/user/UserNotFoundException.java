43445eacb5fcd233d4fdad5b0ab9bc1dbc96dded
Isabelle Bustamante <isabelle.bustamante@students.fhnw.ch>
2022-11-21 21:18:24 +0100

    Update UserNotFoundException.java

--- src/main/java/com/example/mywebapplication/user/UserNotFoundException.java
+++ src/main/java/com/example/mywebapplication/user/UserNotFoundException.java
@@ -1,2 +1,7 @@
-package com.example.mywebapplication.user;public class UserNotFoundException {
+package com.example.mywebapplication.user;
+
+public class UserNotFoundException extends Throwable {
+    public UserNotFoundException(String message) {
+        super(message);
+    }
 }
